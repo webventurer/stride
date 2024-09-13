6a56c05d2817ec11d7ccccd850203af92e48e34e
Mike Mindel <mike@webventurer.com>
2024-09-13 12:40:53 +0100

    Fix 'project_root' variable
    For some reason the `project_root` is pointing to a directory outside of
    the project.  This is now fixed.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 import sys
 
-project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+project_root = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, project_root)
