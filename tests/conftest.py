072338ca77c7242c5cc3d964ad2e8df8ac44b76a
Mike Mindel <mike@webventurer.com>
2024-09-28 17:06:59 +0100

    Remove -> None return type

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,6 @@ NO_TESTS_COLLECTED = 5
 SUCCESS = 0
 
 
-def pytest_sessionfinish(session: pytest.Session, exitstatus: int) -> None:
+def pytest_sessionfinish(session: pytest.Session, exitstatus: int):
     if exitstatus == NO_TESTS_COLLECTED and session.config.getoption("-k"):
         session.exitstatus = SUCCESS
